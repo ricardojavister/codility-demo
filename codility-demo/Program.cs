1dc135359ea368d1f24487a1e6341ff6c1f7c835
Ricardo Torres <ricardojavister@gmail.com>
2020-09-11 23:20:50 -0300

    first commit

---
+++ codility-demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace codility_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int res = 1;
+            A = A.OrderBy(x => x).ToArray();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] <= 0)
+                    continue;
+                else if (A[i] == res)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
